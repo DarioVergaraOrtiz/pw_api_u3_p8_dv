31c08a520e6efd56d27ad8b4412da75b609414f2
DarioVergaraOrtiz <dcvergara@uce.edu.ec>
2025-06-26 07:54:20 -0500

    Tarea 8

--- src/main/java/uce/edu/web/api/controller/ProfesorController.java
+++ src/main/java/uce/edu/web/api/controller/ProfesorController.java
@@ -1,7 +1,13 @@
 package uce.edu.web.api.controller;
 
+import java.util.List;
+
 import jakarta.inject.Inject;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PATCH;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import uce.edu.web.api.repository.modelo.Profesor;
@@ -14,8 +20,35 @@ public class ProfesorController {
     private IProfesorService profesorService;
 
     @GET
-    @Path("/consultar/{id}")
+    @Path("/{id}")
     public Profesor consultarPorId(@PathParam("id") Integer id) {
         return this.profesorService.buscarPorId(id);
+    }
+
+    @GET
+    @Path("")
+    public List<Profesor> consultarTodos(){
+        return this.profesorService.consultarTodos();
+    }
+
+    @POST
+    @Path("")
+    public void guardar(Profesor profesor) {
+        
+    }
+
+    @PUT
+    @Path("/{id}")
+    public void actualizarPorId(@PathParam("id") Integer id,Profesor profesor){
+    }
+
+    @PATCH
+    @Path("/{id}")
+    public void actualizarParcialPorId(@PathParam("id") Integer id,Profesor profesor){
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public void eliminarPorId(@PathParam("id") Integer id) {
     }
 }
