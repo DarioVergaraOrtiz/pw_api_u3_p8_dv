547e8510acebcfd62e2b66334223cba8b7655279
DarioVergaraOrtiz <dcvergara@uce.edu.ec>
2025-07-03 15:51:31 -0500

    Tarea 11

--- src/main/java/uce/edu/web/api/controller/ProfesorController.java
+++ src/main/java/uce/edu/web/api/controller/ProfesorController.java
@@ -2,7 +2,10 @@ package uce.edu.web.api.controller;
 
 import java.util.List;
 
+import org.eclipse.microprofile.openapi.annotations.Operation;
+
 import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.PATCH;
@@ -10,6 +13,9 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import uce.edu.web.api.repository.modelo.Profesor;
 import uce.edu.web.api.service.IProfesorService;
 
@@ -21,33 +27,51 @@ public class ProfesorController {
 
     @GET
     @Path("/{id}")
-    public Profesor consultarPorId(@PathParam("id") Integer id) {
-        return this.profesorService.buscarPorId(id);
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response consultarPorId(@PathParam("id") Integer id) {
+        return Response.status(Response.Status.OK).entity(this.profesorService.buscarPorId(id)).build();
     }
 
     @GET
     @Path("")
-    public List<Profesor> consultarTodos(){
-        return this.profesorService.consultarTodos();
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(
+        summary= "Consultar profesores",
+        description= "Esta capacidad permite consultar profesores de la base de datos"
+    )
+    public Response consultarTodos(){
+        return Response.status(Response.Status.OK).entity(this.profesorService.consultarTodos()).build();
     }
 
     @POST
     @Path("")
-    public void guardar(Profesor profesor) {
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(
+        summary= "Guardar profesor",
+        description= "Esta capacidad permite guardar en la base de datos un profesor"
+    )
+    public Response guardar(Profesor profesor) {
         this.profesorService.guardar(profesor);
+        return Response.status(Response.Status.CREATED).build();
     }
 
     @PUT
     @Path("/{id}")
-    public void actualizarPorId(@PathParam("id") Integer id,Profesor profesor){
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response actualizarPorId(@PathParam("id") Integer id, Profesor profesor){
         profesor.setId(id);
         this.profesorService.actualizarPorId(profesor);
+        return Response.ok().build();
     }
 
     @PATCH
     @Path("/{id}")
-    public void actualizarParcialPorId(@PathParam("id") Integer id,Profesor profesor){
-         profesor.setId(id);
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response actualizarParcialPorId(@PathParam("id") Integer id, Profesor profesor){
+        profesor.setId(id);
         Profesor e = this.profesorService.buscarPorId(id);
         if (profesor.getApellido() != null) {
             e.setApellido(profesor.getApellido());            
@@ -62,11 +86,14 @@ public class ProfesorController {
             e.setFechaContratacion(profesor.getFechaContratacion());
         }
         this.profesorService.actualizarParcialPorId(e);
+        return Response.ok().build();
     }
 
     @DELETE
     @Path("/{id}")
-    public void eliminarPorId(@PathParam("id") Integer id) {
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response eliminarPorId(@PathParam("id") Integer id) {
         this.profesorService.eliminarPorId(id);
+        return Response.noContent().build();
     }
 }
